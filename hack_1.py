dfeb71f9c1c48f58b21dfdfe6c0f9a313b308528
hebertjosedev <hebertjosedev@gmail.com>
2023-10-27 23:39:23 -0300

    feat : hacks hechos

--- hack_1.py
+++ hack_1.py
@@ -5,8 +5,21 @@ text: "fooziman" output => "fOozIman"
 text: "qux" output => "qUx" 
 text: "eq" output => "eq" 
 """
+    
 
+def fn_hack_1(palabra):
+    if len(palabra) < 3:return palabra
+    palabra = list(palabra)
+    rango_inicial= 1
+    pivote = rango_inicial
+    for k in range(len(palabra)):
+        if rango_inicial == 0:
+            palabra[k] = palabra[k].upper()
+            rango_inicial = pivote + 1
+            pivote = rango_inicial
+        else:
+            rango_inicial -=1
 
-def fn_hack_1():
-    result = "fooziman"
-    return result
+    return ("".join(palabra))
+    
+    
